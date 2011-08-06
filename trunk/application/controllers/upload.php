4154a1c55b2321c7ef1a91eda9d0b2bf5b95ce9b
jslim89@gmail.com <jslim89@gmail.com@069d1aaa-2a42-38cc-a8fc-00b45f98e8de>
2011-08-06 05:07:19 +0000

    - create a shell script for changing the folder permission - change the delete file path on UploadHandler - fixed the thumnail directory not exist problem (if not exist, create it)

--- trunk/application/controllers/upload.php
+++ trunk/application/controllers/upload.php
@@ -11,14 +11,36 @@
  * @license GPL Version 3 {@link http://www.gnu.org/licenses/gpl.html}
  */
 class Upload extends MY_Controller {
+
+    protected $upload_url;
+    protected $upload_dir;
+    protected $script_url;
+    protected $upload_handler;
    
     public function __construct() {
         parent::__construct();
-        $this->load->library('upload_handler');
+        $this->load->library('UploadHandler');
+
+        $this->script_url = base_url()."upload";
+        $this->upload_dir = BASEPATH."../images/products/";
+        $this->upload_url = dirname($_SERVER['PHP_SELF']).'/images/products/';
+        $this->upload_options = array(
+            'script_url' => $this->script_url,
+            'upload_dir' => $this->upload_dir,
+            'upload_url' => $this->upload_url,
+            'image_versions' => array(
+                'thumbnail' => array(
+                    'upload_dir' => $this->upload_dir.'thumbnails/',
+                    'upload_url' => $this->upload_url.'thumbnails/',
+                    'max_width' => 80,
+                    'max_height' => 80
+                )
+            )
+        );
+        $this->upload_handler = new UploadHandler($this->upload_options);
     }
 
     public function index() {
-        $upload_handler = new UploadHandler();
 
         header('Pragma: no-cache');
         header('Cache-Control: private, no-cache');
@@ -27,16 +49,20 @@ class Upload extends MY_Controller {
         switch ($_SERVER['REQUEST_METHOD']) {
             case 'HEAD':
             case 'GET':
-                $upload_handler->get();
+                $this->upload_handler->get();
                 break;
             case 'POST':
-                $upload_handler->post();
+                $this->upload_handler->post();
                 break;
             case 'DELETE':
-                $upload_handler->delete();
+                $this->upload_handler->delete();
                 break;
             default:
                 header('HTTP/1.0 405 Method Not Allowed');
         }
+    }
+
+    public function delete($file) {
+        $this->upload_handler->delete($file);
     }
 }
